c70cb1952fd67e1549e1b36f80deabb3ff9a9e2f
Daniel Sedlov <mr.sedlov@gmail.com>
2021-02-08 17:29:50 -0400

    Created Basic Draw Test
    No idea how it works

--- ChemDraw/ChemDraw/ContentView.swift
+++ ChemDraw/ChemDraw/ContentView.swift
@@ -6,27 +6,63 @@
 //
 
 import SwiftUI
+import PencilKit
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 59.85) {
-            Text("Hello, world!")
-                .multilineTextAlignment(.leading)
-                .padding()
-                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
-            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-                .font(.footnote)
-                .fontWeight(.bold)
-                .foregroundColor(Color.black)
-        }
-        .frame(width: 775.096)
-            
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+    }
+}
+
+struct Home : View {
+    @State var canvas  = PKCanvasView()
+    @State var note = UIView();
+    var body: some View{
+        NavigationView{
+//            Draw View
+            NoteView(canvas: $note)
+                .navigationTitle(/*@START_MENU_TOKEN@*/"Cheat Sheet"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.inline/*@END_MENU_TOKEN@*/)
+            DrawingView(canvas: $canvas)
+                .navigationTitle("Drawing")
+                .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.inline/*@END_MENU_TOKEN@*/)
+        }
+        
+    }
+}
+
+struct NoteView : UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    
+//  get drawing for saving
+    @Binding var canvas : UIView
+    func makeUIView(context: Context) ->  UIView {
+        //canvas.drawingPolicy = .anyInput
+        
+        return canvas
+    }
+}
+
+struct DrawingView : UIViewRepresentable {
+    
+//  get drawing for saving
+    @Binding var canvas : PKCanvasView
+    func makeUIView(context: Context) ->  PKCanvasView {
+        canvas.drawingPolicy = .pencilOnly
+        
+        return canvas
+    }
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        
     }
 }
