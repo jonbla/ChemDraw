a6d514c44bf2323368b2b62c6b46f30d40cff203
Daniel Sedlov <mr.sedlov@gmail.com>
2021-02-08 10:42:23 -0400

    SwiftUI Demo

---
+++ ChemDraw/ChemDraw/ChemDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemDrawApp.swift
+//  ChemDraw
+//
+//  Created by Danny Sedlov on 2021-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ChemDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
