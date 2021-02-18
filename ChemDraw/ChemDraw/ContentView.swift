ae3e1deae04caa7df5e3550a61de9ac0798a73e5
Daniel Sedlov <mr.sedlov@gmail.com>
2021-02-18 14:25:04 -0400

    Added List

--- ChemDraw/ChemDraw/ContentView.swift
+++ ChemDraw/ChemDraw/ContentView.swift
@@ -24,55 +24,31 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-//class Canvas: PKCanvasView {
-//
-//  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//    touches.forEach { touch in
-//      print("touch location \(touch.location(in: self))")
-//    }
-//  }
-//
-//}
-
 struct Home : View {
     @State var canvas  = PKCanvasView()
     @State var note = UIView();
     var body: some View{
         
         NavigationView{
-            //            Draw View
-            NoteView(canvas: $note)
-                .navigationTitle("Cheat Sheet")
-                .navigationBarTitleDisplayMode(.inline)
+            List {
+                Note(title: "Note 1")
+            }
+
 //            ScrollView {
-                DrawingView(canvas: $canvas)
-                    .navigationTitle("Drawing")
-                    .navigationBarTitleDisplayMode(.inline)
-                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
-                    .scaledToFill()
-                    .navigationBarItems(trailing: Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Image(systemName: "square.and.arrow.down")
-                    }))
-                    .scaledToFill()
+            DrawingView(canvas: $canvas)
+                .navigationTitle("Drawing")
+                .navigationBarTitleDisplayMode(.inline)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+                .scaledToFill()
+                .navigationBarItems(trailing: Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "square.and.arrow.down")
+                }))
+                .scaledToFill()
 //            }
 //            .scaledToFill()
 //            .ignoresSafeArea()
         }
         .edgesIgnoringSafeArea(.bottom)
-    }
-}
-
-struct NoteView : UIViewRepresentable {
-    func updateUIView(_ uiView: UIView, context: Context) {
-    }
-    
-    
-//  get drawing for saving
-    @Binding var canvas : UIView
-    func makeUIView(context: Context) ->  UIView {
-        //canvas.drawingPolicy = .anyInput
-        
-        return canvas
     }
 }
 
@@ -88,5 +64,15 @@ struct DrawingView : UIViewRepresentable {
     }
     func updateUIView(_ uiView: PKCanvasView, context: Context) {
         
+    }
+}
+
+struct Note: View {
+    let title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.largeTitle)
+            .multilineTextAlignment(.center)
     }
 }
